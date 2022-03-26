a1e2c7cdc3fae088593832810855d361d9d2f58d
nicofant <69219273+nicofant@users.noreply.github.com>
2022-03-26 13:21:23 +0100

    Added the App
    This is needed as there will be no app without it

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Nico Rückner on 06.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var score = 0
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(red: 78/255, green: 84/255, blue: 200/225), Color(red: 143/255, green: 148/255, blue: 251/255)]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+    
+               
+                VStack {
+                    Text("Tap the right flag")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20, weight: .bold))
+                    Text(countries[correctAnswer])
+                        .padding(.bottom)
+                        .foregroundColor(.white)
+                        .font(.system(size: 50, weight: .medium))
+                        
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                    .cornerRadius(10)
+                    // .clipShape(capsule) would make it a capsule shape
+                    .shadow(color: .gray, radius: 5, x: 2, y: 2)
+                }
+                VStack {
+                    
+                    Text("Score: \(score)")
+                        .padding(.top, 120)
+                        .foregroundColor(.white)
+                        .font(.system(size: 20, weight: .bold))
+                }
+            }
+        }
+        
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            askQuestion()
+        } else {
+            
+            askQuestion()
+        }
+        
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nico Rückner on 06.03.22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
